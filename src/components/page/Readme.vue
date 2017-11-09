14b71aba9fbd46b396a58d2fe7cd8dc11f105c91
qc <1758694493@qq.com>
2017-11-09 15:36:27 +0800

    丘嫦提交的Readme.vue

--- src/components/page/Readme.vue
+++ src/components/page/Readme.vue
@@ -1,96 +1,111 @@
 <template>
-    <div>
+    <div class="table">
         <div class="crumbs">
             <el-breadcrumb separator="/">
-                <el-breadcrumb-item><i class="el-icon-setting"></i> 自述</el-breadcrumb-item>
+                <el-breadcrumb-item><i class="el-icon-menu"></i> 当前功能</el-breadcrumb-item>
+                <el-breadcrumb-item class="wd">提交作业</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
-        <div class="ms-doc">
-            <h3>README.md</h3>
-            <article>
-                <h1>manage-system</h1>
-                <p>基于Vue.js 2.x系列 + Element UI 的后台管理系统解决方案</p>
-                <h2>前言</h2>
-                <p>之前在公司用了Vue + Element组件库做了个后台管理系统，基本很多组件可以直接引用组件库的，但是也有一些需求无法满足。像图片裁剪上传、富文本编辑器、图表等这些在后台管理系统中很常见的功能，就需要引用其他的组件才能完成。从寻找组件，到使用组件的过程中，遇到了很多问题，也积累了宝贵的经验。所以我就把开发这个后台管理系统的经验，总结成这个后台管理系统解决方案。</p>
-                <p>该方案作为一套多功能的后台框架模板，适用于绝大部分的后台管理系统（Web Management System）开发。基于vue.js,使用vue-cli脚手架快速生成项目目录，引用Element UI组件库，方便开发快速简洁好看的组件。分离颜色样式，支持手动切换主题色，而且很方便使用自定义主题色。</p>
-                <h2>功能</h2>
-                <el-checkbox disabled checked>Element UI</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>登录/注销</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>表格</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>表单</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>图表</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>富文本编辑器</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>markdown编辑器</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>图片拖拽/裁剪上传</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>支持切换主题色</el-checkbox>
-                <br>
-                <el-checkbox disabled checked>列表拖拽排序</el-checkbox>
-                <br>
-            </article>
+        <div class="btn">
+            <button class="submit">提交</button>
         </div>
-
+        <div class="forms">
+            <el-collapse v-model="activeName" accordion>
+                <el-collapse-item title="调查列表 (+ 单击标题栏展开)" name="1">
+                    <div class="word3">您不是学员，不能提交作业！</div>
+                    <el-form :inline="true" class="form">
+                        <el-form-item style="margin-left: 10px" label="标题">
+                            <el-input size="mini" class="form1" v-model="formLabelAlign.name"></el-input>
+                        </el-form-item>
+                        <el-form-item   label="学员">
+                            <el-input size="mini" class="form1" v-model="formLabelAlign.region"></el-input>
+                        </el-form-item>
+                        <div  style="margin-left: 10px;color: #1f2d3d" class="word4">源代码和文档</div>
+                        <el-upload
+                            class="upload-demo"
+                            drag
+                            action="https://jsonplaceholder.typicode.com/posts/"
+                            multiple>
+                            <i class="el-icon-upload"></i>
+                            <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
+                            <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过500kb</div>
+                        </el-upload>
+                    </el-form>
+                </el-collapse-item>
+            </el-collapse>
+        </div>
+        <div class="btn2">
+            <button class="submit2">确认提交</button>
+            <button class="submit2">重新输入</button>
+        </div>
     </div>
 </template>
-
 <script>
     export default {
-        data: function(){
-            return {}
+        data() {
+            return {
+                labelPosition: 'right',
+                formLabelAlign: {
+                    name: '',
+                    region: ''
+                }
+            };
         }
     }
 </script>
-
-<style scoped>
-    .ms-doc{
-        width:100%;
-        max-width: 980px;
-        font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif;
+<style>
+    .crumbs{
+        width: 980px;
+        height:30px;
     }
-    .ms-doc h3{
-        padding: 9px 10px 10px;
-        margin: 0;
-        font-size: 14px;
-        line-height: 17px;
-        background-color: #f5f5f5;
-        border: 1px solid #d8d8d8;
-        border-bottom: 0;
-        border-radius: 3px 3px 0 0;
+    .btn {
+        background-color: #EEF7FD;
+        border: 1px solid #f2f2f2;
+        width: 1010px;
+        height: 50px;
+        margin-top: -10px;
     }
-    .ms-doc article{
-        padding: 45px;
-        word-wrap: break-word;
-        background-color: #fff;
-        border: 1px solid #ddd;
-        border-bottom-right-radius: 3px;
-        border-bottom-left-radius: 3px;
+    .submit{
+        float: right;
+        margin-right: 10px;
+        margin-top: 15px;
+        color:#1d90e6;
+        border: 1px solid #1d90e6;
+        background-color:  #EEF7FD;
     }
-    .ms-doc article h1{
-        font-size:32px;
-        padding-bottom: 10px;
-        margin-bottom: 15px;
-        border-bottom: 1px solid #ddd;
+    .word3{
+        background-color: yellow;
+        border: 1px solid #ffffff;
+        width: 160px;
+        font-size: 10px;
+        margin-top: 5px;
     }
-    .ms-doc article h2 {
-        margin: 24px 0 16px;
-        font-weight: 600;
-        line-height: 1.25;
-        padding-bottom: 7px;
-        font-size: 24px;
-        border-bottom: 1px solid #eee;
+    .form1{
+        width:250px;
     }
-    .ms-doc article p{
-        margin-bottom: 15px;
-        line-height: 1.5;
+    .form{
+        margin-top: 5px;
+        width:950px;
+        height: 280px;
+        border: 1px solid #d2ecff;
+        margin-left: 13px;
     }
-    .ms-doc article .el-checkbox{
-        margin-bottom: 5px;
+    .word4{
+        font-size:13px;
+        margin-top: -20px;
+    }
+    .btn2{
+        margin-top: 10px;
+    }
+    .submit2{
+        background-color:  #EEF7FD;
+        color:#1d90e6;
+    }
+    .wd{
+        font-weight: bold;
+        font-size: 15px;
+    }
+    .forms{
+        margin-top: 15px;
     }
 </style>
